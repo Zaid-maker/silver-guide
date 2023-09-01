d55cf7a46d4ff71b370883dee020e9387897de5e
Zaid-maker <pzhafeez@gmail.com>
2023-09-01 22:59:44 +0500

    :fire: make `genAllReports` and calll it in HTML

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('Script Loaded')
+async function genAllReports() {
+    const response = await fetch("urls.cfg")
+    const configText = await response.text()
+    const configLines = configText.split("\n")
+
+    for (let ii = 0; ii < configLines.length; ii++) {
+        const configLine = configLines[ii]
+        const [key, url] = configLine.split("=")
+
+        if (!key || !url) {
+            continue
+        }
+    }
+}
